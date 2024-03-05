1b81022cd524f2609ffe862c21accc75133d65a4
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-03-04 21:14:55 -0500

    gate envelope implementation beginning subdivion timing

---
+++ link.bat
@@ -0,0 +1,2 @@
+mklink /J SnailbirdEffects "C:\Square Peg Cyber Dreams Drive\REAPER\Effects\SnailbirdEffects"
+mklink /J SnailbirdScripts "C:\Square Peg Cyber Dreams Drive\REAPER\Scripts\SnailbirdScripts"
