db6ffdd3755dd4985208c0b66bff48ac321b84ed
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-03-09 11:10:18 -0500

    maintenance

--- SnailbirdScripts/include/proj_util.lua
+++ SnailbirdScripts/include/proj_util.lua
@@ -5,7 +5,9 @@ function GetProjectByName(search_name)
   local proj = nil;
   repeat
     proj = reaper.EnumProjects(index);
-    if string.starts(reaper.GetProjectName(proj), search_name) then return true, proj end;
+    if string.starts(reaper.GetProjectName(proj), search_name) then 
+      return true, proj;
+    end;
     index = index + 1;
   until not proj
   return false, proj;
